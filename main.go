245eede4012a5fb0997008b698bdf216d2d783a1
PraNAtaP <pranatapu08@gmail.com>
2026-01-06 01:56:51 +0700

    Struct Data

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import "fmt"
 
-func hitungPersegi(sisi int) (luas int, keliling int) {
-    luas = sisi * sisi
-    keliling = 4 * sisi
-    return 
+// Struct adalah kumpulan dari berbagai tipe data yang dibungkus jadi satu kesatuan
+type Mahasiswa struct {
+    Nama    string
+    NIM     string
+    Jurusan string
+    IPK     float64
 }
 
 func main() {
-    l, k := hitungPersegi(10)
-    fmt.Printf("Luas: %d, Keliling: %d\n", l, k)
+	mhs1 := Mahasiswa{
+		Nama:    "Pranata Putrandana",
+		NIM:     "244107060114",
+		Jurusan: "Teknologi Informasi",
+		IPK:     3.90,
+	}
+	fmt.Printf("Mahasiswa: %s (%s)\n", mhs1.Nama, mhs1.NIM)
+	fmt.Printf("Jurusan: %s (IPK: %.2f)\n", mhs1.Jurusan, mhs1.IPK)
 }
